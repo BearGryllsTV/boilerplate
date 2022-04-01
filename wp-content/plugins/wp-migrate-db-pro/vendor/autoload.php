2af470931e5ccab37422322ecc3e974543290eee
Tom Nightingale <tom@weareleague.co.uk>
2022-04-01 11:22:01 +0100

    update WP Mirgate

--- wp-content/plugins/wp-migrate-db-pro/vendor/autoload.php
+++ wp-content/plugins/wp-migrate-db-pro/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit9b574bf55ac4d39e4d6abf103df21eba::getLoader();
+return ComposerAutoloaderInitd6fa7b6968dfd5b3a45bd1f9ef27e732::getLoader();
--- wp-content/plugins/wp-migrate-db-pro/vendor/scoper-autoload.php
+++ wp-content/plugins/wp-migrate-db-pro/vendor/scoper-autoload.php
@@ -6,15 +6,15 @@ $loader = require_once __DIR__.'/autoload.php';
 
 // Aliases for the whitelisted classes. For more information see:
 // https://github.com/humbug/php-scoper/blob/master/README.md#class-whitelisting
-if (!class_exists('ComposerAutoloaderInit9b574bf55ac4d39e4d6abf103df21eba', false) && !interface_exists('ComposerAutoloaderInit9b574bf55ac4d39e4d6abf103df21eba', false) && !trait_exists('ComposerAutoloaderInit9b574bf55ac4d39e4d6abf103df21eba', false)) {
-    spl_autoload_call('DeliciousBrains\WPMDB\Container\ComposerAutoloaderInit9b574bf55ac4d39e4d6abf103df21eba');
+if (!class_exists('ComposerAutoloaderInitd6fa7b6968dfd5b3a45bd1f9ef27e732', false) && !interface_exists('ComposerAutoloaderInitd6fa7b6968dfd5b3a45bd1f9ef27e732', false) && !trait_exists('ComposerAutoloaderInitd6fa7b6968dfd5b3a45bd1f9ef27e732', false)) {
+    spl_autoload_call('DeliciousBrains\WPMDB\Container\ComposerAutoloaderInitd6fa7b6968dfd5b3a45bd1f9ef27e732');
 }
 
 // Functions whitelisting. For more information see:
 // https://github.com/humbug/php-scoper/blob/master/README.md#functions-whitelisting
-if (!function_exists('composerRequire9b574bf55ac4d39e4d6abf103df21eba')) {
-    function composerRequire9b574bf55ac4d39e4d6abf103df21eba() {
-        return \DeliciousBrains\WPMDB\Container\composerRequire9b574bf55ac4d39e4d6abf103df21eba(...func_get_args());
+if (!function_exists('composerRequired6fa7b6968dfd5b3a45bd1f9ef27e732')) {
+    function composerRequired6fa7b6968dfd5b3a45bd1f9ef27e732() {
+        return \DeliciousBrains\WPMDB\Container\composerRequired6fa7b6968dfd5b3a45bd1f9ef27e732(...func_get_args());
     }
 }
 
